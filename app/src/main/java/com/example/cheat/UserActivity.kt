be5e29d76f2aa299503139444f834de8bd90ee5c
BoZliPraiseTheSun <nitroboy.kap@mail.ru>
2019-07-26 14:59:48 +0300

    Add Google API (No test)
    Add GoogleSingIn (No test)
    Read GoogleFit Calories (No test)

--- app/src/main/java/com/example/cheat/UserActivity.kt
+++ app/src/main/java/com/example/cheat/UserActivity.kt
@@ -1,12 +1,15 @@
 package com.example.cheat
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.renderscript.Element
 import android.text.ParcelableSpan
 import android.util.Log
 import android.view.LayoutInflater
@@ -16,6 +19,12 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.fitness.Fitness
+import com.google.android.gms.fitness.FitnessOptions
+import com.google.android.gms.fitness.data.DataType
+import com.google.android.gms.fitness.data.Field
+import com.google.android.gms.fitness.request.DataReadRequest
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
@@ -23,6 +32,7 @@ import kotlinx.android.synthetic.main.activity_user.*
 import kotlinx.android.synthetic.main.list_product_eat_view.view.*
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
 import kotlin.collections.ArrayList
 
 class UserActivity : AppCompatActivity() {
@@ -34,6 +44,7 @@ class UserActivity : AppCompatActivity() {
         const val SETTINGS_CAL_BURN = "calBurn"
         const val SETTINGS_LIST_EAT_PRODUCTS = "listEatProduct"
         const val SETTINGS_THIS_DAY = "thisDay"
+        const val GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 77
         val listEat: ArrayList<ProductEat> = arrayListOf()
     }
 
@@ -53,6 +64,22 @@ class UserActivity : AppCompatActivity() {
         }
 
         list_eat_recycler.layoutManager = LinearLayoutManager(this)
+
+        val fitnessOptions = FitnessOptions.builder()
+            .addDataType(DataType.TYPE_CALORIES_EXPENDED, FitnessOptions.ACCESS_READ)
+            .addDataType(DataType.AGGREGATE_CALORIES_EXPENDED, FitnessOptions.ACCESS_READ)
+            .build()
+
+        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
+            GoogleSignIn.requestPermissions(
+                this,
+                GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
+                GoogleSignIn.getLastSignedInAccount(this),
+                fitnessOptions
+            )
+        } else {
+            accessGoogleFit()
+        }
 
         nextDay()
         getListEat()
@@ -83,10 +110,59 @@ class UserActivity : AppCompatActivity() {
         }
     }
 
+    fun accessGoogleFit() {
+        val calendar = Calendar.getInstance()
+        calendar.time = Date()
+        val endTime = calendar.timeInMillis
+        calendar.add(Calendar.HOUR_OF_DAY, -calendar.get(Calendar.HOUR_OF_DAY))
+        val startTime = calendar.timeInMillis
+
+        val builder = DataReadRequest.Builder()
+            .aggregate(DataType.TYPE_CALORIES_EXPENDED, DataType.AGGREGATE_CALORIES_EXPENDED)
+            .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+            .build()
+
+        Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this)!!)
+            .readData(builder)
+            .addOnSuccessListener {
+                Log.d(TAG, "accessGoogle Success")
+            }
+            .addOnFailureListener {
+                Log.d(TAG, "accessGoogle Failure")
+            }
+            .addOnCompleteListener {
+                Log.d(TAG, "accessGoogle Complete")
+                var burnCal = 0f
+                if (it.result?.buckets!!.size > 2) {
+                    val buck = it.result!!.buckets[1]
+                    val ds = buck.getDataSet(DataType.AGGREGATE_CALORIES_EXPENDED)
+                    for (dp in ds!!.dataPoints) {
+                        val avg = dp.getValue(Field.FIELD_CALORIES).asFloat()
+                        Log.d(TAG, "avg: $avg")
+                        burnCal += avg
+                    }
+                }
+                Log.d(TAG, "burnCal: $burnCal")
+                mSettings.edit().putFloat(
+                    SETTINGS_CAL_BURN,
+                    mSettings.getFloat(SETTINGS_CAL_BURN, 0f) + burnCal
+                ).apply()
+            }
+    }
+
     @SuppressLint("SimpleDateFormat")
     fun getData(): Int {
         val formatData = SimpleDateFormat("DD")
         return formatData.format(Date()).toInt()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when(requestCode) {
+                GOOGLE_FIT_PERMISSIONS_REQUEST_CODE -> {accessGoogleFit()}
+            }
+        }
     }
 
     override fun onResume() {
